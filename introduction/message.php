cb9e840f6b42c9747d71b32cbf27f3b28b7eeeac
Manisha Yadav <53181670+maneeshay@users.noreply.github.com>
2021-10-26 20:58:32 +0545

    Add files via upload

---
+++ introduction/message.php
@@ -0,0 +1,7 @@
+<?php
+
+// Print some words to the screen
+
+print "If all is well then this message will be seen when you browse this web page";
+
+?>
---
+++ introduction/test.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+
+    <title>My web document</title>
+
+    <link href="test.css" rel="stylesheet" type="text/css" />
+
+</head>
+
+<body>
+
+    <section id="container">
+
+        <p>
+
+            Below is a PHP generated message:-
+
+        </p>
+
+        <?php
+
+        include "message.php";
+
+?>
+
+    </section>
+
+</body>
+
+</html>
